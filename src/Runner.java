cf18b163b91cc96e5b036b29161d791a7a0dccd1
Sunnyok <sunxlite@gmail.com>
2011-12-16 00:41:46 -0500

    expand

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,17 @@
+/*
+Class Name: Runner.java
+Author: Sunny Li, Leo Liu
+Date: Dec 16, 2011
+School: A.Y. Jackson SS
+Computer used: Sunny's Computer
+IDE used: Notepad++
+Purpose: This is the runner class which runs the main program.
+*/
+
 public class Runner{
+	//Main method
 	public static void main(String[] args){
+		//Test message
 		System.out.println("Program starter...");
 		Music run = new Music(); //create one instance of music...
 	}
