351d1ce8e2954d00619a0aa76aa90058353b7381
Sunnyok <sunxlite@gmail.com>
2011-12-16 01:00:05 -0500

    XML reading

---
+++ src/XML/OpenXML.java
@@ -0,0 +1,21 @@
+/*
+Class Name: OpenXML.java
+Author: Sunny Li, Leo Liu
+Date: Dec 16, 2011
+School: A.Y. Jackson SS
+Computer used: Sunny's Computer
+IDE used: Notepad++
+Purpose: Read in XML file data
+*/
+
+import javax.xml.parsers.DocumentBuilderFactory; //Used to Create DocumentBuilder
+import javax.xml.parsers.DocumentBuilder; //Used to build document
+import org.w3c.dom.*; //Document Object Model is an API to read XML document
+import java.io.File; //File IO
+
+public class OpenXML{
+	public static void main(String[] args){
+		//Going to sleep...
+		System.out.println("Under Construction...");
+	}
+}
